3dd0c11b55c7d82ced53d410d0b5f493aa2eba37
pixel-Teee <1173769234@qq.com>
2022-10-18 14:19:10 +0800

    read chapter 12.1

--- Template/Template/Chapter12.h
+++ Template/Template/Chapter12.h
@@ -2,6 +2,7 @@
 
 namespace Note12do1
 {
+	//------definitions1------
 	template<typename T> //a namespace scope class template
 	class Data
 	{
@@ -23,4 +24,28 @@ namespace Note12do1
 
 	template<typename T> //a namespace scope alias template
 	using DataList = Data<T*>;
+	//------definitions1------
+
+	//------definitions2------
+	class Collection
+	{
+	public:
+		template<typename T> //an in-class member class template definition
+		class Node
+		{
+			//...
+		};
+
+		template<typename T> //an in-class(and therefore implicitly inline)
+		T* alloc() {		//member function template definition
+			//...
+		};
+
+		template<typename T> //a member variable template(since C++14)
+		static T zero = 0;
+
+		template<typename T> //a member alias template
+		using NodePtr = Node<T>*;
+	};
+	//------definitions2------
 }
